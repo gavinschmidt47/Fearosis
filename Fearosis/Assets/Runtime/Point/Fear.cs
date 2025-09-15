8066e05102bc49e814e9481ebb6ad1dc5dcdf4ea
Gavin Schmidt <51172158+gavinschmidt47@users.noreply.github.com>
2025-09-15 11:31:34 -0400

    Update Fear.cs

--- Fearosis/Assets/Runtime/Point/Fear.cs
+++ Fearosis/Assets/Runtime/Point/Fear.cs
@@ -8,39 +8,11 @@ public class Fear : Point
     [SerializeField]
     private Prejudice prejudice;
 
-    //Portion of Fear points to send to Notoriety and Prejudice
-    private float curNVal; //base notoriety value
-    private float curPVal; //base prejudice value
-
+    //Percent of Fear points gained to send to Notoriety and Prejudice
     [Range(0f, 1f)]
     public float notorietyModifier;
-
     [Range(0f, 1f)]
     public float prejudiceModifier;
-
-    public void Start() //sets starting values for notoriety/prejudice
-    {
-        curNVal = notorietyModifier;
-        curPVal = prejudiceModifier;
-    }
-
-    public void OnValidate() //keeps the values linked
-    {
-        if (notorietyModifier != curNVal) //notoriety, change prejudice
-        {
-            prejudiceModifier = (1 - notorietyModifier); //adjust the prejudice modifier 
-            curNVal = notorietyModifier;
-            curPVal = prejudiceModifier; //reset the base value to stop checking and adjusting
-        }
-        
-        else if (prejudiceModifier != curPVal) //prejudice, change notoriety
-        {
-            notorietyModifier = (1 - prejudiceModifier); //adjust the prejudice modifier 
-            curPVal = prejudiceModifier;
-            curNVal = notorietyModifier; //reset the base value to stop checking and adjusting
-        } 
-
-    }
 
     //Gain Fear points from various sources
     public override void GainPoints(int pointsToGain, string source)
