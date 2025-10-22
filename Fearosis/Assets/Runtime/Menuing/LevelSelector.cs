dfbcf61625a89738ed8fa8544fb8ef81b7b89dfe
Gavin Schmidt <51172158+gavinschmidt47@users.noreply.github.com>
2025-10-22 11:57:52 -0400

    Level Select Setup

--- Fearosis/Assets/Runtime/Menuing/LevelSelector.cs
+++ Fearosis/Assets/Runtime/Menuing/LevelSelector.cs
@@ -1,78 +1,43 @@
+using System.ComponentModel;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class LevelSelector : MonoBehaviour
 {
-    public bool infiniteModeOn = false;
-    public PlayLevel playLevelScript;
+    public Button mainButton;
 
-    public TMPro.TextMeshProUGUI descriptionBox;
+    public GameObject buttonsPanel;
 
-    [Header("Use this to type in scenes to use")]
-    [Header("For Normal Mode")]
-    public string normalScene;
-    [Header("For Infinite Mode")]
-    public string infiniteScene;
-
-    [Header("Respective gamemode option buttons")]
     public Button normalMode;
     public Button infiniteMode;
+    public GameObject descriptionBox;
 
-    [Header("All Share the same play button")]
-    public Button playButton;
-    RectTransform descTransform;
+    public Sprite impossibleIcon;
+    public Sprite lockedImpossibleIcon;
 
-    float currentX;
-    float currentY;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        playButton.interactable = false;
-        descTransform = descriptionBox.GetComponent<RectTransform>();
-        normalMode.gameObject.SetActive(false);
-        infiniteMode.gameObject.SetActive(false);
+        buttonsPanel.SetActive(false);
 
-        currentX = descTransform.anchoredPosition.x;
-        currentY = descTransform.anchoredPosition.y;
+        descriptionBox.SetActive(true);
+
+        mainButton.onClick.AddListener(ShowGamemodes);
+
+        normalMode.onClick.AddListener(() =>
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(this.gameObject.name);
+        });
+        infiniteMode.onClick.AddListener(() =>
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(this.gameObject.name + " Infinite");
+        });
     }
 
     public void ShowGamemodes()
     {
-        //move description text down
-        if (!normalMode.gameObject.activeSelf)
-        {
-            descTransform.anchoredPosition = new Vector2(currentX, currentY-70.0f);
-        }
-        else
-        {
-            descTransform.anchoredPosition = new Vector2(currentX, currentY);
-        }
-
-        //reveal the normal mode and infinite mode buttons
-        normalMode.gameObject.SetActive(!normalMode.gameObject.activeSelf);
-        infiniteMode.gameObject.SetActive(!infiniteMode.gameObject.activeSelf);
-        
+        buttonsPanel.SetActive(!buttonsPanel.activeSelf);
+        descriptionBox.SetActive(!descriptionBox.activeSelf);
     }
-
-    public void SelectNormalMode()
-    {
-        if (infiniteModeOn)
-        {
-            infiniteModeOn = false;
-        }        
-        playButton.interactable = true;
-        playLevelScript.normalScene = this.normalScene;
-    }
-
-    public void SelectInfiniteMode()
-    {
-        if (!infiniteModeOn)
-        {
-            infiniteModeOn = true;
-        }
-        playButton.interactable = true;
-        playLevelScript.infiniteScene = this.infiniteScene;
-    }
-
 }
