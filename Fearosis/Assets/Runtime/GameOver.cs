9defdfb671e28bbc23a0d8aaf3aee4a55c75aa58
SDrastal <sa122032@ucf.edu>
2025-11-21 13:43:56 -0500

    Win and loss has been added, as well as sprites sorted

--- Fearosis/Assets/Runtime/GameOver.cs
+++ Fearosis/Assets/Runtime/GameOver.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
     private FullGameStats fullGameStatsScript;
+
+    [SerializeField] private GameObject winScreen;
+    [SerializeField] private GameObject loseScreen;
 
     public void CheckGameOver()
     {
@@ -10,11 +14,26 @@ public class GameOver : MonoBehaviour
 
         if (fullGameStatsScript.infected >= fullGameStatsScript.population)
         {
+            winScreen.SetActive(true);
+            ResetGame();
             //WIN
         }
         if (fullGameStatsScript.infected <= 0)
         {
+            loseScreen.SetActive(true);
+            ResetGame();
             //LOSE
         }
+    }
+
+    public void ResetGame()
+    {
+        StartCoroutine(ResetGameCoroutine());
+    }
+
+    private System.Collections.IEnumerator ResetGameCoroutine()
+    {
+        yield return new WaitForSeconds(7f);
+        SceneManager.LoadScene("LevelSelect");
     }
 }
